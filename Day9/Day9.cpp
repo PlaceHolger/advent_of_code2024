65e4777249c089bd8d87486a6f112bec9408c09e
PlaceHolger <hxpb@gmx.de>
2024-12-10 14:48:00 +0100

    Day9 Part2

--- Day9/Day9.cpp
+++ Day9/Day9.cpp
@@ -8,6 +8,8 @@
 
 constexpr int EMPTY_SPACE = -666;
 
+//#define IS_PART1
+
 // std::string ReadDataFromFile()
 // {
 //     std::ifstream file("data.txt");
@@ -61,6 +63,7 @@ int main(int argc, char* argv[])
     //     i == EMPTY_SPACE ? std::cout << "." : std::cout << i;
     // }
 
+#if defined(IS_PART1)
     //now we remove the empty spaces from the result, we do so by filling them with the numbers from the end of the vector
     uint64_t currentPosFromBack = result.size() - 1;
     for (uint64_t i=0; i < result.size() && i < currentPosFromBack; ++i)
@@ -74,7 +77,70 @@ int main(int argc, char* argv[])
         }
     }
     result.resize(currentPosFromBack + 1);
-
+#else
+    //in part 2 we also fill the empty spaces, but we only move whole file blocks instead of single numbers
+    //we first create a list with free blocks, then we start at the end of the vector and move the blocks to the empty spaces
+    struct Block
+    {
+        int index;
+        int size;
+    };
+    std::vector<Block> freeBlocks;
+    for (int i = 0; i < result.size(); ++i)
+    {
+        if (result[i] == EMPTY_SPACE)
+        {
+            int blockSize = 1;
+            while (result[i + blockSize] == EMPTY_SPACE)
+            {
+                blockSize++;
+            }
+            freeBlocks.push_back({i, blockSize});
+            i += blockSize;
+        }
+    }
+    //we do the same for the data blocks, but start at the end of the vector
+    std::vector<Block> dataBlocks;
+    for (int i = result.size() - 1; i >= 0; )
+    {
+        if (result[i] != EMPTY_SPACE)
+        {
+            int currentId = result[i];
+            int blockSize = 1;
+            while (i - blockSize > 0 && result[i - blockSize] == currentId)
+            {
+                blockSize++;
+            }
+            i -= blockSize;
+            dataBlocks.push_back({i + 1, blockSize});
+        }
+        else
+        {
+            i--;
+        }
+    }
+    //now we iterate over the data blocks and fill the empty spaces with the data blocks, if a block is too big, we skip it. we move to the first empty space that is large enough (can also be larger, in that case we adjust the block size is the free list)
+    for (const auto& dataBlock : dataBlocks)
+    {
+        for (auto& freeBlock : freeBlocks)
+        {
+            if (freeBlock.index >= dataBlock.index)
+                break;
+            
+            if (freeBlock.size >= dataBlock.size)
+            {
+                for (int i = 0; i < dataBlock.size; ++i)
+                {
+                    result[freeBlock.index + i] = result[dataBlock.index + i];
+                    result[dataBlock.index + i] = EMPTY_SPACE;
+                }
+                freeBlock.size -= dataBlock.size;
+                freeBlock.index += dataBlock.size;
+                break;
+            }
+        }
+    }
+#endif
     // //test print cleaned up string
     // std::cout << '\n' << "After Cleaning: ";
     // for (const auto& i : result)
@@ -86,7 +152,8 @@ int main(int argc, char* argv[])
     uint64_t score = 0;
     for (uint64_t i = 0; i < result.size(); ++i)
     {
-        score += result[i] * i;
+        if (result[i] != EMPTY_SPACE)
+            score += result[i] * i;
     }
     std::cout << '\n' << "filesystem checksum: " << score << '\n';
     
